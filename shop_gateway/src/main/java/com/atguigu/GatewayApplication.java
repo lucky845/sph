0f1576a45c0f8c657fcaa5e948692477f7bad214
lucky845 <lc1447545564@gmail.com>
2022-04-05 23:27:04 +0800

    搭建Gateway网关，并处理全局跨域

---
+++ shop_gateway/src/main/java/com/atguigu/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lucky845
+ * @date 2022年04月05日 23:03
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
