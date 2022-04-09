3b362d681b76ec5dc2546c42910e8f0d2404ec56
lucky845 <lc1447545564@gmail.com>
2022-04-09 11:30:31 +0800

    修复页面小bug

--- shop_core/shop_product/src/main/java/com/atguigu/controller/ConcurrentController.java
+++ shop_core/shop_product/src/main/java/com/atguigu/controller/ConcurrentController.java
@@ -3,9 +3,9 @@ package com.atguigu.controller;
 import com.atguigu.service.ConcurrentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -17,7 +17,7 @@ import javax.annotation.Resource;
  */
 @Api(tags = "测试Redis作为分布式锁")
 @RequestMapping("/product")
-@Controller
+@RestController
 public class ConcurrentController {
 
     @Resource
