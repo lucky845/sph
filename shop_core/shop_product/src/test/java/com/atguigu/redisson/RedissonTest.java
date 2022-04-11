51313884f363c89b92fb444b94c7ff3f1c4d1a23
lucky845 <lc1447545564@gmail.com>
2022-04-11 09:37:05 +0800

    修复bug

--- shop_core/shop_product/src/test/java/com/atguigu/redisson/RedissonTest.java
+++ shop_core/shop_product/src/test/java/com/atguigu/redisson/RedissonTest.java
@@ -1,6 +1,7 @@
 package com.atguigu.redisson;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 
