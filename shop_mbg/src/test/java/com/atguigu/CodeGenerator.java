7d54e2e2943b6c3085a9a1ab34986fab5be50c50
lucky845 <lc1447545564@gmail.com>
2022-04-27 21:02:48 +0800

    项目部署到阿里云(shop-product模块)

--- shop_mbg/src/test/java/com/atguigu/CodeGenerator.java
+++ shop_mbg/src/test/java/com/atguigu/CodeGenerator.java
@@ -37,7 +37,7 @@ public class CodeGenerator {
 
         // 3、数据源配置
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
-        dataSourceConfig.setUrl("jdbc:mysql://192.168.229.128:3306/shop_seckill?serverTimezone=GMT%2B8&useSSL=false");
+        dataSourceConfig.setUrl("jdbc:mysql://120.79.10.205:3306/shop_seckill?serverTimezone=GMT%2B8&useSSL=false");
         dataSourceConfig.setDriverName("com.mysql.jdbc.Driver");
         dataSourceConfig.setUsername("root");
         dataSourceConfig.setPassword("root");
